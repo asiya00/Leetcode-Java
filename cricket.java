3bae0eb138ace726f43b271e44ab25f94fe489a2
asiya00 <asiyapathan2000@gmail.com>
2022-09-16 15:59:06 +0530

    TreeMap Problem

--- cricket.java
+++ cricket.java
@@ -107,8 +107,9 @@ class TreemapHandson {
 public class cricket{
   public static void main(String[] args) {
     TreemapHandson th = new TreemapHandson();
-    // String answer = th.getQuery("1,Rohit,100|2,Virat,56|3,Dhoni,150\n3,Rahul,90|2,Virat,100|1,Rohit,99\n1,Rahul,50|2,Virat,64|3,Rohit,78\n1,Dhoni,95|2,Virat,30|3,Rohit,45\n1,Jadeja,43|2,Virat,54|3,Rohit,78|4,Dhoni,59\n1,Rahul,60|2,Virat,56|3,Rohit,69", "3");
-    String answer = th.getQuery("3,Dhoni,100|1,Virat,50|5,Jadeja,90|2,Rohit,70|4,Pandya,30", "2 50");
+    // String answer=th.getQuery("3,Dhoni,100|1,Virat,50|5,Jadeja,90|2,Rohit,70|4,Pandya,30", "1 2 4"); // 1-querytype, 2-start position, 4-endposition. In dataset firstparameter is position
+    // String answer = th.getQuery("1,Rohit,100|2,Virat,56|3,Dhoni,150\n3,Rahul,90|2,Virat,100|1,Rohit,99\n1,Rahul,50|2,Virat,64|3,Rohit,78\n1,Dhoni,95|2,Virat,30|3,Rohit,45\n1,Jadeja,43|2,Virat,54|3,Rohit,78|4,Dhoni,59\n1,Rahul,60|2,Virat,56|3,Rohit,69", "3"); // 3 - querytype to find efficient opener. In dataset firstparameter is number of matches.
+    String answer = th.getQuery("3,Dhoni,100|1,Virat,50|5,Jadeja,90|2,Rohit,70|4,Pandya,30", "2 50"); //2-Querytype, 50-threshold score
     System.out.println(answer);
   }
 }
